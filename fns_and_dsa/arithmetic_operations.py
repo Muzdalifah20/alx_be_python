52d7709d98405a6fc61611956205659170d430b4
Muzdalifah <muzdalifahsuliman@gmail.com>
2025-07-27 14:04:54 +0300

    simple modification

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,13 +1,16 @@
-def perform_operation(num1 , num2 , operation):
-    match operation :
-        case "add" :
-            return num1 + num2
-        case "subtract" :
-            return num1 - num2
-        case "multiply" :
-            return num1 * num2
-        case "divide" :
-            if num2 == 0 :
-                print("can't divide by 0")
-            else:
-                return num1 / num2
+def perform_operation(num1, num2, operation):
+    if operation == "add":
+        return num1 + num2
+    elif operation == "subtract":
+        return num1 - num2
+    elif operation == "multiply":
+        return num1 * num2
+    elif operation == "divide":
+        if num2 == 0:
+            print("can't divide by 0")
+            return None
+        else:
+            return num1 / num2
+    else:
+        print("Invalid operation")
+        return None
